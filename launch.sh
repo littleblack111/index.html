36e9fbac37f8492783fd88a5afdf698355679d1c
littleblack111 <littleblack11111@gmail.com>
2023-10-03 20:07:56 +0800

    -m

--- launch.sh
+++ launch.sh
@@ -2,7 +2,8 @@ printf '\033[0;35mCode Checking Starts...\033[0m\n'
 html5validator --also-check-css --also-check-svg --verbose | npx gnomon || printf '\033[0;31mCode Check Failed...\033[0m\n'
 # sleep .5
 echo
-htmltest --conf ./.htmltest.yml | npx gnomon
-printf '\033[0;32mNpx Server Started...\033[0m\n'
+htmltest --conf ./.htmltest.yml | npx gnomon || printf '\033[0;31mCode Check Failed...\033[0m\n'
+# sleep .5
+printf '\033[0;32mNpx Server Starting...\033[0m\n'
 
-exec npx serve #| npx gnomon;
+exec npx serve -d -p 209 #| npx gnomon;
