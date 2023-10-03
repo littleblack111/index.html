8e188e172c42d65d61d03582dd4949844ad18a0c
littleblack111 <littleblack11111@gmail.com>
2023-10-03 19:27:38 +0800

    so all POSIX system work

--- launch.sh
+++ launch.sh
@@ -1,8 +1,8 @@
-echo '\033[0;35mCode Checking Starts...\033[0m'
-html5validator --also-check-css --also-check-svg --verbose | npx gnomon || echo '\033[0;31mCode Check Failed...\033[0m'
+printf '\033[0;35mCode Checking Starts...\033[0m\n'
+html5validator --also-check-css --also-check-svg --verbose | npx gnomon || printf '\033[0;31mCode Check Failed...\033[0m\n'
 # sleep .5
 echo
 htmltest --conf ./.htmltest.yml | npx gnomon
-echo '\033[0;32mNpx Server Started...\033[0m'
+printf '\033[0;32mNpx Server Started...\033[0m\n'
 
 exec npx serve #| npx gnomon;
