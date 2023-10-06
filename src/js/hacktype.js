c93459a5a4bc2d21bd35cf2d4b25f5024e310716
littleblack111 <littleblack11111@gmail.com>
2023-10-06 23:22:07 +0800

    adjustment to interval of typing of hacktype.js

--- src/js/hacktype.js
+++ src/js/hacktype.js
@@ -29,8 +29,8 @@ document.addEventListener("DOMContentLoaded", () => {
             clearInterval(intervals.shift());
           }
 
-          iteration += 1 / 3;
-        }, 30)
+          iteration += 1 / 5;
+        }, 20)
       );
     });
   }
