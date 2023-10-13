1acb1eda4c47f9bc0f04a03aa3fbad17e5864432
littleblack111 <littleblack11111@gmail.com>
2023-10-13 08:38:52 +0800

    additional features like auto switch per min

--- src/js/rbg.js
+++ src/js/rbg.js
@@ -1,3 +1,5 @@
+document.body.style.transition = "background 1s ease-in-out";
+
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
   let color = "#";
@@ -8,17 +10,32 @@ function getRandomColor() {
 }
 
 function setRandomGradient() {
-  const rand3 = Math.floor(Math.random());
+  const rand3 = Math.floor(Math.random() * 3) + 1;
   if (rand3 === 1) {
-    document.body.style.background = `linear-gradient(${Math.floor(Math.random() * 180)}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+    document.body.style.background = `linear-gradient(${Math.floor(
+      Math.random() * 180
+    )}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
   } else if (rand3 === 2) {
-    document.body.style.background = `linear-gradient(${Math.floor(Math.random() * 180)}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}),
-linear-gradient(${Math.floor(Math.random() * 180)}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+    document.body.style.background = `linear-gradient(${Math.floor(
+      Math.random() * 180
+    )}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}),
+linear-gradient(${Math.floor(
+      Math.random() * 180
+    )}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
   } else {
-      document.body.style.background = `linear-gradient(${Math.floor(Math.random() * 180)}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}),
-linear-gradient(${Math.floor(Math.random() * 180)}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}),
-linear-gradient(${Math.floor(Math.random() * 180)}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+    document.body.style.background = `linear-gradient(${Math.floor(
+      Math.random() * 180
+    )}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}),
+linear-gradient(${Math.floor(
+      Math.random() * 180
+    )}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}),
+linear-gradient(${Math.floor(
+      Math.random() * 180
+    )}deg, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
   }
 }
 
 setRandomGradient();
+
+// Execute setRandomGradient() every 1 minute
+setInterval(setRandomGradient, 60000);
