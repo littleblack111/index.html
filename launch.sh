6dce4e2e8ed35a26af389b7ab388113381350444
littleblack111 <littleblack11111@gmail.com>
2023-10-14 16:20:25 +0800

    added functions to build, start

--- launch.sh
+++ launch.sh
@@ -1,4 +1,4 @@
-function selfcheck() {
+function check() {
   printf '\033[0;35mCode Checking Starts...\033[0m\n'
   html5validator --also-check-css --also-check-svg --verbose | npx gnomon || printf '\033[0;31mCode Check Failed...\033[0m\n'
   # sleep .5
@@ -7,13 +7,25 @@ function selfcheck() {
   # sleep .5
 }
 
+function buildcss() {
+  printf '\033[0;35mCode CSS Build Starts...\033[0m\n'
+  npm run build:css || printf '\033[0;31mCode CSS Build Failed...\033[0m\n'
+}
+
+function buildjs() {
+  printf '\033[0;35mCode JavaScript Compile Starts...\033[0m\n'
+  npm run build:js || printf '\033[0;31mCode JavaScript Compile Failed...\033[0m\n'
+}
+
 function start() {
   printf '\033[0;32mNpx Server Starting...\033[0m\n'
   exec npx serve -d -p 209 #| npx gnomon;
 }
 
 function main() {
-  selfcheck
+  check
+  buildcss
+  buildjs
   start
 }
 
