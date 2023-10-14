bd9536ee06890c07456337fec3e7a916e03d8f6e
littleblack111 <littleblack11111@gmail.com>
2023-10-14 22:27:54 +0800

    re build index.js

--- dest/js/index.js
+++ dest/js/index.js
@@ -1,13 +1,1 @@
 "use strict";
-
-var obj = document.getElementById('infos');
-var topBar = document.getElementById('navBarHeader');
-window.addEventListener('scroll', function () {
-  var value = window.scrollY;
-  obj.style.marginTop = value * 2.5 + 'px';
-  console.log("==============");
-  console.log(topBar.style.height);
-  console.log(value);
-}, {
-  passive: true
-});
