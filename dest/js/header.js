5151fe758d60f7695415e3eccc3b2c717ed7550f
littleblack111 <littleblack11111@gmail.com>
2023-10-29 01:34:21 +0800

    re-build

--- dest/js/header.js
+++ dest/js/header.js
@@ -5,6 +5,6 @@ var newElement = document.createElement('div');
 var page = header.getAttribute('page');
 
 // console.log(page);
-newElement.innerHTML = "\n<!-- header -->\n<header id=\"navBarHeader\">\n    <nav class=\"navBar\">\n        <div class=\"hleft\">\n            <a id=\"btnHome\" style=\"text-decoration: unset\" href=\"/\">Home<span></span></span></a>\n        </div>\n        <div class=\"hright\">\n            <a href=\"/pages/repo/github/repos.html\" id=\"btnGhRepo\" style=\"text-decoration: unset\">Github Repos</a>\n            <a href=\"/pages/info/about.html\" id=\"btnAbout\" style=\"text-decoration: unset\">About</a>\n        </div>\n    </nav> \n</header>\n";
+newElement.innerHTML = "\n<!-- header -->\n<header id=\"navBarHeader\">\n\t<nav class=\"navBar\">\n\t\t<div class=\"hleft\">\n\t\t\t<a id=\"btnHome\" style=\"text-decoration: unset\" href=\"/\">Home<span></span></span></a>\n\t\t</div>\n\t\t<div class=\"hright\">\n\t\t\t<a href=\"/pages/repo/github/repos.html\" id=\"btnGhRepo\" style=\"text-decoration: unset\">Github Repos</a>\n\t\t\t<a href=\"/pages/info/about.html\" id=\"btnAbout\" style=\"text-decoration: unset\">About</a>\n\t\t</div>\n\t</nav> \n</header>\n";
 header.appendChild(newElement);
 document.getElementById("btn" + page).setAttribute('class', 'active');
