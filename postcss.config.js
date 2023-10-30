f138c963257b36263c1abc38fb032bff3105eaa0
littleblack111 <littleblack11111@gmail.com>
2023-10-30 16:46:17 +0800

    added plugin: postcssImport, postcssNested

--- postcss.config.js
+++ postcss.config.js
@@ -10,18 +10,22 @@ const pixrem = require('pixrem')
 const doiuse = require('doiuse')
 const postcssFixes = require('postcss-fixes')
 const rucksackCss = require('rucksack-css')
+const postcssImport = require('postcss-import')
+const postcssNested = require('postcss-nested')
 
 module.exports = {
     plugins: [
-      cssnano({ preset: 'default' }),
-      postcssPresetEnv(),
-      postcssWillChange(),
-      autoprefixer(),
-      postcssColorRgbaFallback(),
-      postcssPseudoelements(),
-      postcssVmin(),
-      doiuse(),
-      postcssFixes(),
-      rucksackCss(),
+		cssnano({ preset: 'default' }),
+		postcssPresetEnv(),
+		postcssWillChange(),
+		autoprefixer(),
+		postcssColorRgbaFallback(),
+		postcssPseudoelements(),
+		postcssVmin(),
+		doiuse(),
+		postcssFixes(),
+		rucksackCss(),
+		postcssImport(),
+		postcssNested(),
     ],
 };
