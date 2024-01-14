7913376ead61159f4a591748e207a57b6fe2a9ea
littleblack111 <littleblack11111@gmail.com>
2024-01-14 12:20:35 +0800

    un-comment code

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,10 @@
-// if ('serviceWorker' in navigator) {
-//     window. addEventListener ('load', () => {
-//         navigator.serviceWorker.register('/sw.js')
-//             .then (reg => {
-//                 console.log('sw.js Registered', reg);
-//             }) .catch(err => {
-//                 console.log('sw.js failed: ', err);
-//         });
-//     });
-// }
+if ('serviceWorker' in navigator) {
+    window. addEventListener ('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then (reg => {
+                console.log('sw.js Registered', reg);
+            }) .catch(err => {
+                console.log('sw.js failed: ', err);
+        });
+    });
+}
