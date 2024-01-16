53496e0a05586aa80ea21e0b71a3b3c665f64b5a
littleblack111 <littleblack11111@gmail.com>
2024-01-16 11:01:37 +0800

    remove console.log when sucess

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,7 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('/sw.js').then(function (reg) {
-      console.log('sw.js Registered', reg);
-    }).catch(function (err) {
+    navigator.serviceWorker.register('/sw.js').then(function (reg) {}).catch(function (err) {
       console.log('sw.js failed: ', err);
     });
   });
--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ if ('serviceWorker' in navigator) {
     window. addEventListener ('load', () => {
         navigator.serviceWorker.register('/sw.js')
             .then (reg => {
-                console.log('sw.js Registered', reg);
+                // console.log('sw.js Registered', reg);
             }) .catch(err => {
                 console.log('sw.js failed: ', err);
         });
