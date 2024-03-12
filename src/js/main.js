1514ac74f31055a9fe810e40020760dfaa9d373e
littleblack111 <littleblack11111@gmail.com>
2024-03-12 12:29:23 +0800

    fixed format

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,9 @@
 if ('serviceWorker' in navigator) {
-    window. addEventListener ('load', () => {
+    window.addEventListener ('load', () => {
         navigator.serviceWorker.register('/sw.js')
             .then (reg => {
                 // console.log('sw.js Registered', reg);
-            }) .catch(err => {
+            }).catch(err => {
                 console.log('sw.js failed: ', err);
         });
     });
