ff253a30044553da0fcd6372fd32500eb55759a1
littleblack111 <littleblack11111@gmail.com>
2024-03-27 15:42:58 +0800

    moved scripts load here

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,30 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
+
+function loadScript(src, async, defer) {
+    var script = document.createElement('script');
+    script.src = src;
+    script.async = async;
+    script.defer = defer;
+    document.body.appendChild(script);
+}
+
+function loadModuleScript(src, async, defer) {
+    var script = document.createElement('script');
+    script.src = src;
+    script.type = "module"
+    script.async = async;
+    script.defer = defer;
+    document.body.appendChild(script);
+}
+
+loadScript('/js/header.js', '', 'true')
+
+
+loadScript('/js/transition.js', '', 'true')
+
+window.va = window.va || function () { (window.vaq = window.vaq || []).push(arguments); };
+loadScript('/_vercel/insights/script.js', '', 'true')
+
+loadModuleScript('/js/swup.js', 'true')
